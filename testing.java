3fe1ebe0b4cf6a31335b9151447ab5f7293e831a
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-06-24 08:25:24 -0300

    Create testing.java

---
+++ testing.java
@@ -0,0 +1,18 @@
+package misc;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Simulate s = new Simulate(10);
+		s.start_simulation();
+	
+		
+	
+}
+	
+	
+}	
